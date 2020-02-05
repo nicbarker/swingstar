f54347549a7e70b3fd89b5765806503dc545815b
Nic Barker <nic.b@canva.com>
2020-02-05 21:35:53 +1300

    Add utility for capturing screenshots

---
+++ Assets/Scripts/CaptureScreenshotScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureScreenshotScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log(Application.persistentDataPath);
+            ScreenCapture.CaptureScreenshot("Screenshot1" + Random.Range(1, 1000).ToString() + ".png");
+        }
+    }
+}
